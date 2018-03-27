381fe53583ef5a703d71eb2eaf580e049f756770
Ishu Singh <ishus1242@gmail.com>
2018-03-28 00:21:58 +0530

    Added functionality in Transactions that will filter credit and debit transactions from transaction list.

--- library/src/test/java/com/step/Bank/TransactionsTest.java
+++ library/src/test/java/com/step/Bank/TransactionsTest.java
@@ -4,8 +4,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -18,7 +17,7 @@ public class TransactionsTest {
     private Transactions transactions;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         transactions = new Transactions();
     }
 
@@ -45,12 +44,53 @@ public class TransactionsTest {
 
     @Test
     public void filterTransactionsByAmount() {
+        transactions.credit(1001,"Neeraj");
+        transactions.credit(11,"Pragya");
+        transactions.debit(1001,"Pragya");
+        CreditTransaction credit = new CreditTransaction(1001, "Neeraj");
+        DebitTransaction debit = new DebitTransaction(1001,"Pragya");
+        Transactions filteredTransactions = transactions.filterByAmountGreaterThan(1000);
+        assertThat(filteredTransactions.list,hasItems(credit,debit));
+    }
+
+
+    @Test
+    public void printsTheTransactionInToString() throws FileNotFoundException, UnsupportedEncodingException {
+        transactions.debit(1000, "Arvind");
+        transactions.credit(1000, "Debu");
+        CreditTransaction credit = new CreditTransaction(new Date(), 1000, "Debu");
+        DebitTransaction debit = new DebitTransaction(new Date(),1000,"Arvind");
+        ArrayList<String> result = new ArrayList<>();
+        PrintWriter writer = new PrintWriter("file.txt","UTF-8"){
+            @Override
+            public void println(String x) {
+                result.add(x);
+            }
+        };
+        this.transactions.print(writer);
+        writer.close();
+        assertThat(result,hasItems(credit.toString(),debit.toString()));
+    }
+
+    @Test
+    public void filtersAllCreditTransactions() {
         transactions.credit(1000,"Neeraj");
-        transactions.credit(1500,"Pragya");
+        transactions.credit(15000,"Pragya");
         transactions.debit(150,"Pragya");
-        CreditTransaction credit = new CreditTransaction(1000, "Debu");
-        DebitTransaction debit = new DebitTransaction(1000,"Arvind");
-        Transactions transaction = transactions.filterByAmountGreaterThan(1000);
-        System.out.println(transaction);
+        CreditTransaction credit1 = new CreditTransaction(1000, "Neeraj");
+        CreditTransaction credit2 = new CreditTransaction(15000, "Pragya");
+        Transactions transaction = transactions.filterAllCreditTransactions();
+        assertThat(transaction.list,hasItems(credit1,credit2));
+    }
+
+    @Test
+    public void filtersAllDebitTransactions() {
+        transactions.debit(1000,"Pragya");
+        transactions.credit(15000,"Pragya");
+        transactions.debit(15000,"Anjali");
+        DebitTransaction debit1 = new DebitTransaction(1000, "Pragya");
+        DebitTransaction debit2 = new DebitTransaction(15000, "Anjali");
+        Transactions transaction = transactions.filterAllDebitTransactions();
+        assertThat(transaction.list,hasItems(debit1,debit2));
     }
 }
